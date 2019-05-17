f6520470160d00a9a02a1e44b87e3382a86e79d8
fenet <fenetgucci@gmail.com>
2019-05-17 08:35:22 +0300

    modified

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,10 +1,4 @@
 //= require active_admin/base
-
-
- src="https://www.googletagmanager.com/gtag/js?id=UA-140005874-1"
-
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
-
- gtag('config', 'UA-140005874-1');
+document.addEventListener('turbolinks:load', function() {
+  gtag('config', 'UA-140005874-1')
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-document.addEventListener('turbolinks:load', function() {
-  gtag('config', 'UA-140005874-1')
-});
