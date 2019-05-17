607ff917b475d94da13a722db77edbae712a37f7
fenet <fenetgucci@gmail.com>
2019-05-17 08:31:38 +0300

    modified

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,4 +1,10 @@
 //= require active_admin/base
 
 
- 
+ src="https://www.googletagmanager.com/gtag/js?id=UA-140005874-1"
+
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+
+ gtag('config', 'UA-140005874-1');
